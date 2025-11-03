9b524249446fe106b629a289877e07b45733a4a9
ccdude <ccdude1016@gmail.com>
2025-11-03 20:53:33 +0800

    Create ex3-7.py

---
+++ ex3-7.py
@@ -0,0 +1,24 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+# 整數 (Integer)
+age = input("please input int number = ")
+print("age = ",age)
+print(type(age))
+
+age = int(input("please input int number = "))
+print("age = ",age)
+print(type(age))
+# 浮點數 (Float)
+height = input("please input float number = ")
+print("height = ",height)
+print(type(height))
+
+height = float(input("please input float number = "))
+print("height = ",height)
+print(type(height))

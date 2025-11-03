48910e01b6247f6ae295a62649645056cb78a8e6
ccdude <ccdude1016@gmail.com>
2025-11-03 19:51:03 +0800

    Create ex3-3.py

---
+++ ex3-3.py
@@ -0,0 +1,23 @@
+'''
+# 字串 (String)
+name = "20"
+message = '歡迎使用 Python'
+
+# 整數 (Integer)
+age = 13
+score = 100 
+
+# 浮點數 (Float)
+height = 169.5
+price = 99.99
+
+#不林值 (Boolean)
+is_student = True
+has_license = False
+
+# 檢查變數類型
+print(name)
+print(type(name))       # <class 'str'>
+print(type(age))        # <class 'int'>
+print(type(height))     # <class 'float'>
+print(type(is_student)) # <class 'bool'>

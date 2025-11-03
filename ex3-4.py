8a55217ea7353cda1927b3f94e7e145ea93ee03b
ccdude <ccdude1016@gmail.com>
2025-11-03 20:10:25 +0800

    Create ex3-4.py

---
+++ ex3-4.py
@@ -0,0 +1,19 @@
+'''
+# 字串 (String)
+name = "20"
+
+print(name)
+print(type(name))       # <class 'str'>
+# 整數 (Integer)
+age = 13
+name = 20
+print(type(age))        # <class 'int'>
+print(name)
+print(type(name))       # <class 'str'>
+# 浮點數 (Float)
+height = 169.5
+
+print(type(height))     # <class 'float'>
+#不林值 (Boolean)
+is_student = True
+print(type(is_student)) # <class 'bool'>

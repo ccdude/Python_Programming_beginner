e9c43cf92195676a051ad347775b9a8ddb49dd80
ccdude <ccdude1016@gmail.com>
2025-11-17 19:32:19 +0800

    Create ex5-1.py

---
+++ ex5-1.py
@@ -0,0 +1,8 @@
+'''
+print(f"加法:{a} + {b} = {a + b}")
+print(f"減法:{a} - {b} = {a - b}")
+print(f"乘法:{a} * {b} = {a * b}")
+print(f"除法:{a} / {b} = {a / b}")
+print(f"整數除法:{a} // {b} = {a // b}")
+print(f"餘數:{a} % {b} = {a & b}")
+print(f"次方:{a} ** {b} = {a ** b}")
